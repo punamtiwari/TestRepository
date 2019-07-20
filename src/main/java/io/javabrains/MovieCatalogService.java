5e33c3ef21c5df8d3214502b395f01f32f56379e
punamtiwari <kmpr4@yahoo.com>
2019-07-20 18:53:08 -0400

    its my first commit

---
+++ src/main/java/io/javabrains/MovieCatalogService.java
@@ -0,0 +1,18 @@
+package io.javabrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogService {
+
+	public static void main(String[] args) {
+		
+		System.out.println("watching movie");
+		SpringApplication.run( MovieCatalogService.class,args);
+			
+		}
+	 
+	}
+
+
